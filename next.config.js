398054a09b8c078d11a6893eaa8ca794945356f4
Groshan67 <ghassemroshan@gmail.com>
2024-08-04 14:48:30 +0330

    change localhost path2

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboard",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
